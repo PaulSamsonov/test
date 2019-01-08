37e993914fd903358187a597aca9a788d6c026bd
dmitriy.k.itlt <dmitriy.k.itlt@gmail.com>
2019-01-08 13:06:29 +0200

    validation, recaptcha, switcher for block on popup and landing

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function ($) {
-  // Variable
+  //----- Variable
   var failList = ['algerian', 'bahamian', 'bangladeshi', 'bolivian', 'batswana', 'ethiopian', 'chinese', 'cuban', 'ecuadorean', 'ghanaian', 'iranian', 'jordanian', 'macedonian', 'moroccan', 'nepalese', 'north_korean', 'pakistani', 'serbian', 'sri_lankan', 'syrian', 'sudanese', 'trinidadian_or_tobagonian', 'tunisian', 'american', 'venezuelan', 'yemeni'
-  ], email, firstName, lastName, nationality, tokenButtons, toNewsletter;
+  ], email, firstName, lastName, nationality, tokenButtons, toNewsletter, termsAndConditions;
 
-  // Events
+  //----- Events
 
   //anchor links
   $('.main-nav a[href*="#"], .anchor').click(function (event) {
@@ -16,38 +16,84 @@ jQuery(document).ready(function ($) {
   // Add contact to global list and other lists
   $('#getWhitelisted').on('click', function (e) {
     e.preventDefault();
-    email = $('#email').val();
-    firstName = $('#firstName').val();
-    lastName = $('#lastName').val();
-    nationality = $('#nationality').val();
+    email = $('#email');
+    firstName = $('#firstName');
+    lastName = $('#lastName');
+    nationality = $('#nationality');
     tokenButtons = $('#tokenButtons .active').text();
     toNewsletter = $('#addToNews').prop('checked');
+    termsAndConditions = $('#termsAndCond');
 
-    addToAllContacts();
+    var error = false;
+
+    if (!email.val().length) {
+      error = true;
+      email.addClass('error');
+    } else {
+      email.removeClass('error');
+    }
+
+    if (!firstName.val().length) {
+      error = true;
+      firstName.addClass('error');
+    } else {
+      firstName.removeClass('error');
+    }
+
+    if (!lastName.val().length) {
+      error = true;
+      lastName.addClass('error');
+    } else {
+      lastName.removeClass('error');
+    }
+
+    if (!nationality.val().length) {
+      error = true;
+      nationality.addClass('error');
+    } else {
+      nationality.removeClass('error');
+    }
+
+    if (!termsAndConditions.prop('checked')) {
+      error = true;
+      termsAndConditions.addClass('error');
+      $('label[for="termsAndCond"]').addClass('error');
+      console.log('termsAndConditions');
+    } else {
+      email.removeClass('error');
+    }
+
+    if (!error) {
+      addToAllContacts();
+    }
 
   });
 
-  // Switch investor type
+  // Switch investor type on popup
   $('#tokenButtons').on('click', '.token-button', function (e) {
     e.preventDefault();
-
-    if (!$(this).hasClass('active')) {
-      $('#tokenButtons .active').removeClass('active');
-      $(this).addClass('active');
-    }
+    var landBlock = $('#tokenButtonsLanding');
+    switchInvestorType(this, landBlock);
   });
 
-  // Functions
+  // Switch investor type on landing page
+  $('#tokenButtonsLanding').on('click', '.token-button', function (e) {
+    e.preventDefault();
+    var landBlock = $('#tokenButtons');
+    switchInvestorType(this, landBlock);
+  });
+
+  //----- Functions
 
   // Add contact to global list
   function addToAllContacts () {
 
     var arr = [],
       obj = {
-        email: email,
-        first_name: firstName,
-        last_name: lastName,
-        nationality: nationality,
+        email: email.val(),
+        first_name: firstName.val(),
+        last_name: lastName.val(),
+        nationality: nationality.val(),
         investor_type: tokenButtons
       };
     arr.push(obj);
@@ -96,4 +142,19 @@ jQuery(document).ready(function ($) {
     });
   }
 
+  // Switch investor type
+  function switchInvestorType (self, landBlock) {
+    landBlock.find('.active').removeClass('active');
+
+    if (!$(self).hasClass('active')) {
+      $(self).parent().find('.active').removeClass('active');
+      $(self).addClass('active');
+
+      if($(self).hasClass('more')){
+        landBlock.find('.more').addClass('active');
+      } else {
+        landBlock.find('.less').addClass('active');
+      }
+    }
+  }
 });
